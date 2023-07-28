db2fbf58ac3ea3e05da386fd0087df3e7e4e60fe
unknown <emailpabs@gmail.com>
2023-07-27 20:00:56 -0500

    Updated html, css and js.

--- script.js
+++ script.js
@@ -1,79 +1,158 @@
-// The startGame function is called when the start button is clicked
-// function startGame() {
-//   isWin = false;
-//   timerCount = 10;
-//   startTimer()
-// }
+var startButton = document.getElementById('startButton');
 
-// Array of correct answers
-// var Correct = ["variable","array", "modulus", "object", "function", "string", "boolean"];
+let timerCount=90
+var timerDisplay = document.getElementById('timerDisplay');
+let timer;
+let quizContainer = document.querySelector('.quiz-container');
+let question = document.querySelector('#question')
+let option1 = document.getElementById("option1")
+let option2 = document.getElementById("option2")
+let option3 = document.getElementById("option3")
+let option4 = document.getElementById("option4")
+let correct = document.getElementById("correct")
+option1.addEventListener('click', checkButton)
+option2.addEventListener('click', checkButton)
+option3.addEventListener('click', checkButton)
+option4.addEventListener('click', checkButton)
 
-// The init function is called when the page loads 
-// function init() {
-//   getCorrect();
-//   getWrong();
-// }
-// multiple choice
-// maintain wrong & correct via local storage
+quizContainer.style.display = "none"
+startButton.addEventListener('click', function() {
+  console.log('Start button clicked')
+  startQuizTimer()
+  startButton.style.display = 'none'
+  quizContainer.style.display = "block"
+  displayQuestion()
+})
 
-// var wordBlank = document.querySelector(".word-blanks");
-// var win = document.querySelector(".win");
-// var lose = document.querySelector(".lose");
-// var timerElement = document.querySelector(".timer-count");
-// var startButton = document.querySelector(".start-button");
+function startQuizTimer() {
 
-// var chosenWord = "";
-// var numBlanks = 0;
-// var winCounter = 0;
-// var loseCounter = 0;
-// var isWin = false;
-// var timer;
-// var timerCount;
+  timerCount = 20;
+  timer = setInterval(function() {
+    timerDisplay.innerText="Time: " + timerCount
+    timerCount--;
 
-// The answer function is called when the win condition is met
-// function answer() {
-//   wordBlank.textContent = "YOU WON!!!🏆 ";
-//   winCounter++
-//   startButton.disabled = false;
-//   setWins()
-// }
+    if (timerCount <= 0) {
+  
+        endQuiz()
+        // "endQuiz()" Calculate and present "Your final score is 30.", enter initials and submit.
+        // Highscore page with options to Go Back and ClearHighscores
+        // Go Back button = return to init
 
-// The timeout function is called when timer reaches 0
-// function endGame() {
-//   present score and enter initials then press submit
-//   list scores in ascending order, high on top
-// }
+    }
+  }, 1000);
+}
+var questionsList = [
+  {
+    question:"What day comes after Monday?",
+    option1:"1. Monday",
+    option2:"2. Tuesday",
+    option3:"3. Wednesday",
+    option4:"4. Thursday",
+    answer:"2. Tuesday"
+  },
+  {
+    question:"What day comes before Wednesday?",
+    option1:"1. Monday",
+    option2:"2. Tuesday",
+    option3:"3. Wednesday",
+    option4:"4. Thursday",
+    answer:"2. Tuesday"
+  },
+  {
+    question:"What day comes after Sunday?",
+    option1:"1. Monday",
+    option2:"2. Tuesday",
+    option3:"3. Wednesday",
+    option4:"4. Thursday",
+    answer:"1. Monday"
+  },
+  {
+    question:"What day comes before Tuesday?",
+    option1:"1. Monday",
+    option2:"2. Tuesday",
+    option3:"3. Wednesday",
+    option4:"4. Thursday",
+    answer:"1. Monday"
+  },
+  {
+    question:"What day comes after Wednesday?",
+    option1:"1. Monday",
+    option2:"2. Tuesday",
+    option3:"3. Wednesday",
+    option4:"4. Thursday",
+    answer:"4. Thursday"
+  },
+]
+var currentQuestion = 0;
+var score = 0;
+function displayQuestion(){
+  question.innerText = questionsList[currentQuestion].question
+  option1.innerText = questionsList[currentQuestion].option1
+  option2.innerText = questionsList[currentQuestion].option2
+  option3.innerText = questionsList[currentQuestion].option3
+  option4.innerText = questionsList[currentQuestion].option4
+}
 
-// // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-// function startTimer() {
-//   // Sets timer
-//   timer = setInterval(function() {
-//     timerCount--;
-//     timerElement.textContent = timerCount;
-//     if (timerCount >= 0) {
-//       // Tests if win condition is met
-//       if (isWin && timerCount > 0) {
-//         // Clears interval and stops timer
-//         clearInterval(timer);
-//         winGame();
-//       }
-//     }
-//     // Tests if time has run out
-//     if (timerCount === 0) {
-//       // Clears interval
-//       clearInterval(timer);
-//       loseGame();
-//     }
-//   }, 1000);
-// }
+function checkButton(event){
+  var userInput = event.target.innerText
+  console.log(userInput)
+  if (userInput === questionsList[currentQuestion].answer) {
+    score++
+    correct.innerText = "Correct!"
+  } else{
+    timerCount -= 5
+    correct.innerText = "Wrong!"
+  }
+  if (currentQuestion < questionsList.length - 1){
+    currentQuestion++ 
+     displayQuestion()
+  }else{
+    endQuiz()
+  }
+}
+
+function endQuiz () {
+  clearInterval(timer);
+  quizContainer.style.display = "none"
+}
+// 
 
 // Attach event listener to start button to call startGame function on click
 // startButton.addEventListener("click", startQuiz);
 
+// var startQuiz = document.querySelector(".start-button");
+
+// The startGame function is called when the start button is clicked
+// 
+// 
+// The init function is called when the page loads 
+// function init() {
+//   getCorrect();
+//   getWrong();
+//   set in local storage
+// }
+// multiple choice
+// 
+// var timerElement = document.querySelector(".timer-count");
+// 
+// var correct = "";
+// var wrong = "";
+// 
+// var timer;
+// var timerCount;
+
+// The endQuiz function is called when all questions are answered or time expires
+
+// function endQuiz() {
+// Generate score based on Correct!(true) and Wrong!(false) 
+// answers stored in local storage
+// present score and enter initials then press submit
+// list scores in ascending order, high on top
+// } 
+// 
 // Calls init() so that it fires when page opened
 // init();
-
-// Bonus: Add reset button
+// 
 // var clearHighScores = document.querySelector(".clearHighScores-button");
 
 // function Clear Highscores() {
@@ -81,3 +160,4 @@
 // }
 // Attaches event listener to button
 // resetButton.addEventListener("click", clearHighScores);
+// 
